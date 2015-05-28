7a5fe6a59360829ce3281978ac7d6db018251162
Brit Butler <redline6561@gmail.com>
2015-05-28 16:43:51 -0400

    Add method to get a single user.

--- lib/scrapr/github.rb
+++ lib/scrapr/github.rb
@@ -6,6 +6,9 @@ module Scrapr
     base_uri 'https://api.github.com'
     basic_auth ENV['GH_USER'], ENV['GH_PASS']
 
-    
+
+    def get_user(username)
+      self.class.get("/users/#{username}")
+    end
   end
 end
