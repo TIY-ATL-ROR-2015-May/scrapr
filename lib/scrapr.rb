fa2958f83036cbe69b46a37bc7b4c23858fbe739
Brit Butler <redline6561@gmail.com>
2015-05-28 16:48:39 -0400

    Add not quite finished App class.

--- lib/scrapr.rb
+++ lib/scrapr.rb
@@ -8,6 +8,31 @@ require 'scrapr/github'
 require 'scrapr/user'
 
 module Scrapr
+  class App
+    def initialize
+      @github = Github.new
+    end
+
+    def prompt(question, validator)
+      puts question
+      input = gets.chomp
+      until input =~ validator
+        puts "Sorry, wrong answer."
+        puts question
+        input = gets.chomp
+      end
+      input
+    end
+
+    def run
+      username = prompt("What username would you like to download?", /^\w+$/)
+      user_data = @github.get_user(username)
+      User.create(user_data)
+    end
+  end
 end
 
 binding.pry
+
+# app = Scrapr::App.new
+# app.run
