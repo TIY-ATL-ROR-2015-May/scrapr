7c40af1c94ba165f3730de51ce9316836d1d16db
Brit Butler <redline6561@gmail.com>
2015-05-29 14:38:33 -0400

    Add OAuth example from class.

--- lib/scrapr/github.rb
+++ lib/scrapr/github.rb
@@ -5,23 +5,30 @@ module Scrapr
   # Environment Variable with ENV['ACCESS_TOKEN']
 
   # Either way, at least you're not using a real password. :)
-  ACCESS_TOKEN = ''
+  ACCESS_TOKEN = ENV['ACCESS_TOKEN']
 
   class Github
     include HTTParty
     base_uri 'https://api.github.com'
 
     def initialize
-      @auth = { "Authorization" => "token #{ACCESS_TOKEN}",
-                "User-Agent" => "HTTParty" }
+      @headers = { "Authorization" => "token #{ACCESS_TOKEN}",
+                   "User-Agent" => "HTTParty" }
     end
 
     def get_user(username)
-      self.class.get("/users/#{username}", headers: @auth)
+      self.class.get("/users/#{username}", headers: @headers)
     end
 
-    def get_contributions(owner, repo)
-      self.class.get("/repos/#{owner}/#{repo}/stats/contributors", headers: @auth)
+    def get_contributions(owner, repo, page=1)
+      params = {
+        page: page
+      }
+      options = {
+        headers: @headers,
+        query: params
+      }
+      self.class.get("/repos/#{owner}/#{repo}/stats/contributors", options)
     end
   end
 end
