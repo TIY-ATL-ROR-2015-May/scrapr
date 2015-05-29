f233c3c066e28e6cff3e7770268b5b334c5d708e
Brit Butler <redline6561@gmail.com>
2015-05-29 11:25:01 -0400

    Add OAuth token/HTTParty headers hackery.

--- lib/scrapr/github.rb
+++ lib/scrapr/github.rb
@@ -1,14 +1,27 @@
 require 'httparty'
 
 module Scrapr
+  # You could hardcode this or grab it from an
+  # Environment Variable with ENV['ACCESS_TOKEN']
+
+  # Either way, at least you're not using a real password. :)
+  ACCESS_TOKEN = ''
+
   class Github
     include HTTParty
     base_uri 'https://api.github.com'
-    basic_auth ENV['GH_USER'], ENV['GH_PASS']
 
+    def initialize
+      @auth = { "Authorization" => "token #{ACCESS_TOKEN}",
+                "User-Agent" => "HTTParty" }
+    end
 
     def get_user(username)
-      self.class.get("/users/#{username}")
+      self.class.get("/users/#{username}", headers: @auth)
+    end
+
+    def get_contributions(owner, repo)
+      self.class.get("/repos/#{owner}/#{repo}/stats/contributors", headers: @auth)
     end
   end
 end
